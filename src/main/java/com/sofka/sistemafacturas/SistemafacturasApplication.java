5af21c292e2fd5d77250158859a4ad27dbfd07f4
juangil991 <jcgo125@gmail.com>
2022-03-30 14:50:25 -0500

    commit inicial

---
+++ src/main/java/com/sofka/sistemafacturas/SistemafacturasApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.sistemafacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemafacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemafacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/sistemafacturas/SistemafacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.sistemafacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
